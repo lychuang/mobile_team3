b4d09ec57fdb7536b9be2fb03b211081e3db2f92
tramjn <nguyenrg@bc.edu>
2018-11-13 01:26:20 +0900

    clickable CardView

--- app/src/main/java/com/example/lachlanhuang/buskerbusker/FeedAdapter.java
+++ app/src/main/java/com/example/lachlanhuang/buskerbusker/FeedAdapter.java
@@ -1,11 +1,15 @@
 package com.example.lachlanhuang.buskerbusker;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import static android.support.v4.content.ContextCompat.startActivity;
 
 public class FeedAdapter extends RecyclerView.Adapter {
 
@@ -15,6 +19,8 @@ public class FeedAdapter extends RecyclerView.Adapter {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_item, parent, false);
         return new FeedViewHolder(view);
     }
+
+
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
@@ -29,9 +35,18 @@ public class FeedAdapter extends RecyclerView.Adapter {
     private class FeedViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mUserName;
 
-        public FeedViewHolder(View itemView) {
+        public FeedViewHolder(final View itemView) {
             super(itemView);
             mUserName = (TextView) itemView.findViewById(R.id.item_username);
+
+            // transition to post
+            itemView.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    Intent intent = new Intent(itemView.getContext(), FeedPost.class);
+                    itemView.getContext().startActivity(intent);
+
+                }
+            });
         }
 
         public void bindView(int position) {
---
+++ app/src/main/java/com/example/lachlanhuang/buskerbusker/FeedPost.java
@@ -0,0 +1,14 @@
+package com.example.lachlanhuang.buskerbusker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedPost extends AppCompatActivity {
+
+    // TODO: Create page for when you click on each post
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_post);
+    }
+}
