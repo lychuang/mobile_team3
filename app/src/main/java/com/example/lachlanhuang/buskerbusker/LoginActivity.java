11dc5275f195bb78ee95e6560766d8ab76683cfb
Lachlan Huang <lachlan.huang@uqconnect.edu.au>
2018-11-13 20:42:49 +0900

    You can now sign up! give it a try, I think passwords need to be at least 6 chars for firebase

--- app/src/main/java/com/example/lachlanhuang/buskerbusker/LoginActivity.java
+++ app/src/main/java/com/example/lachlanhuang/buskerbusker/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.lachlanhuang.buskerbusker;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +12,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.lachlanhuang.buskerbusker.database.User;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -28,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText mEmail, mPassword;
     private Button btnSignIn,btnSignOut,btnSignUp;
 
-    private User user;
+    private User mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        user = new User();
+        mUser = new User();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -66,17 +69,10 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String email = mEmail.getText().toString();
-                String pass = mPassword.getText().toString();
-                if(!email.equals("") && !pass.equals("")){
-                    mAuth.signInWithEmailAndPassword(email,pass);
+                String password = mPassword.getText().toString();
+                if(!email.equals("") && !password.equals("")){
 
-                    Log.d(TAG, "onClick: Switching Activities.");
-
-                    user.setEmail("MOOOOOOO");
-
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    intent.putExtra("USER_CLASS", user);
-                    startActivity(intent);
+                    signIn(email, password);
 
                 }else{
                     toastMessage("You didn't fill in all the fields.");
@@ -96,6 +92,10 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                createAccount(email, password);
             }
         });
     }
@@ -122,4 +122,61 @@ public class LoginActivity extends AppCompatActivity {
     private void toastMessage(String message){
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
+
+
+    private void createAccount(final String email, final String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    signIn(email, password); //sign in and move onto the map
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+                });
+    }
+
+
+    void signIn(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    mUser.setEmail("MOOOOOOO");
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("USER_CLASS", mUser);
+                    startActivity(intent);
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                // ...
+            }
+        });
+    }
+
+
 }
