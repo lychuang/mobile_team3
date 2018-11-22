d5ef53d4ff46ed433f23d8920c55644275e639d4
tramjn <nguyenrg@bc.edu>
2018-11-23 04:22:42 +0900

    set up basic account settings page

--- app/src/main/java/com/example/lachlanhuang/buskerbusker/AccountFragment.java
+++ app/src/main/java/com/example/lachlanhuang/buskerbusker/AccountFragment.java
@@ -6,24 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.preference.PreferenceFragment;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AccountFragment extends Fragment {
+public class AccountFragment extends PreferenceFragmentCompat {
 
 
     public AccountFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+    public void onCreatePreferences(Bundle bundle, String s) {
+        // Load the Preferences from the XML file
+        addPreferencesFromResource(R.xml.app_preferences);
     }
+
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_account, container, false);
+//    }
 
 }
